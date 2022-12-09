766e3c52d44fdb72f44732ff36ae9056cd59594e
Maaz Ali <moaaxali@gmail.com>
2022-12-10 02:16:01 +0500

    Import SQLAlchemy and configure Database uri

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@127.0.0.1:5432/todoapp'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():

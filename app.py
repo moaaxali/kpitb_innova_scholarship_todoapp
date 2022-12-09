84bd065dadff3fcaa0780ad6e6c114504709069b
Maaz Ali <moaaxali@gmail.com>
2022-12-10 02:07:42 +0500

    Add and index.html file to render dummpy data using render_template

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Hello World!'
+  return render_template('index.html', data=[{
+    'description': 'Todo 1'
+  }, {
+    'description': 'Todo 2'
+  }, {
+    'description': 'Todo 3'
+  }])
